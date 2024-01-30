5b6a3bebc73e260ad824caed8691f51a725799c7
Patch <projassainz@gmail.com>
2024-01-30 23:44:20 +0100

    algunos cambios2

--- angular17dreamit/src/app/app.component.ts
+++ angular17dreamit/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
+import { HeaderHomeComponent } from './pages/home/header-home/header-home.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { NavigatorComponent } from './components/navigator/navigator.component';
     HomeComponent,
     FooterComponent,
     NavigatorComponent,
+    HeaderHomeComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
